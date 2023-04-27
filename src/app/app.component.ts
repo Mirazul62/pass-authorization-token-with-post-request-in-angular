3673a4b80d0dacceba45ac8cc748e5ee44961ea5
mirazul <mirazul@julyservices.com>
2023-04-27 15:35:50 +0600

    pass token in angular with post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test_ea';
+  constructor(private http: HttpClient) {}
+  
+  preview(){
+      let model = {
+        planId : '0004010',
+        ssn : '000000067',
+        enrollstatcd: 'abc',
+        enrollsteP1CD: 'abc',
+        enrollsteP2CD: 'abc',
+        enrollsteP3CD: 'abc',
+        enrollsteP4CD : 'abc'
+      };
+      let api_key = "Api_Key_Here";
+      const headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${api_key}`
+        });
+      const requestOptions = { headers: headers };
+      console.log("test");
+      this.http.post('https://localhost:44390/api/enrollmentstatus', model, requestOptions).subscribe((item:any) => {
+        window.location.href=item.enrollmentAppUrl;
+      })
+  }
 }
+  
+
+
+
